cb6cb633aca3f63a4a83bf5adc3a4aa64b386224
GothicLolita229 <90853197+GothicLolita229@users.noreply.github.com>
2021-09-16 11:08:36 -0400

    First upload

---
+++ CTI110_P1LAB1_Linares/main.py
@@ -0,0 +1,11 @@
+#CTI 110
+#P1LAB1
+#Lourdes
+#9.16.21
+
+#This is 7.12 from Zybooks
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Math bug fixed
+   
+print(userNumSquared)       # Output formatting issue fixed
